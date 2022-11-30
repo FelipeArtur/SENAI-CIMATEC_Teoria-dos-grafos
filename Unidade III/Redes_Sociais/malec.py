9b82373ce0351fd4b6572df34fead590638b4a01
Felipe Lima <xpxlipe@gmail.com>
2022-11-30 19:19:06 -0300

    [UPDATE] - Criação de arestas V1
    Desenvolvendo uma forma de criação das arestas

--- Unidade III/Redes_Sociais/malec.py
+++ Unidade III/Redes_Sociais/malec.py
@@ -3,26 +3,51 @@ import matplotlib.pyplot as plt
 import os
 
 grafo = ig.Graph()
-grafo['titulo'] = "Koonfia"
+grafo['titulo'] = "Koonfia_Social"
 exit = False
 fig, ax = plt.subplots(figsize=(15, 15))
 
-def adicionar_usuário():
+
+def adicionar_usuario():
     print("Insira o nome do usuário")
     username = input("> ")
     grafo.add_vertex(name=username)
     print("Usuário "+username+" adicionado!")
 
+
+def adicionar_conexao():
+    edges = [(0, 1), (0, 2), (0, 3), (0, 4), (1, 2), (1, 3), (1, 4), (3, 4)]
+    grafo.add_edges(edges)
+    """""
+    print("Quantas conexões você quer fazer?")
+    num_conexoes = input("> ")
+    while (num_conexoes > 0):
+        grafo.add_edge()
+        num_conexoes = num_conexoes-1
+        print("Deu bom")
+    """
+
+
 def mostrar_grafo():
     print("VISUALIZAÇÃO DA REDE EM ...\n\n")
-    ig.plot(grafo, target=ax, layout="circle", vertex_size=0.1, vertex_color="steelblue", vertex_frame_color="white", vertex_label=grafo.vs["name"])
+    ig.plot(
+        grafo,
+        target=ax,
+        layout="circle",
+        vertex_size=0.1,
+        vertex_color="steelblue",
+        vertex_frame_color="white",
+        vertex_label=grafo.vs["name"])
     plt.show()
 
+
 def clear_system():
-    os.system('cls' if os.name=='nt' else 'clear')
+    os.system('cls' if os.name == 'nt' else 'clear')
+
 
 def pause_system():
     pause = input("Pressione ENTER para continuar!")
+
 
 while True:
     clear_system()
@@ -35,17 +60,19 @@ while True:
     print("6 - Visualizar")
     print("7 - Sair")
     response = int(input("Escolha a opção: > "))
-    
+
     match response:
         case 1:
-            adicionar_usuário()
+            adicionar_usuario()
+        case 2:
+            adicionar_conexao()
         case 4:
             mostrar_grafo()
         case 7:
             exit = True
         case _:
             print("Opção inválida!")
-    
+
     if exit:
         break
 
