f1e96f75a3f934f8838b4a1d9cbcd6464b398b56
Felipe Lima <xpxlipe@gmail.com>
2022-11-30 23:36:33 -0300

    [UPDATE] - Atualização do projeto

--- Unidade III/Redes_Sociais/malec.py
+++ Unidade III/Redes_Sociais/malec.py
@@ -2,34 +2,53 @@ import igraph as ig
 import matplotlib.pyplot as plt
 import os
 
+'''Criação do Grafo'''
 grafo = ig.Graph()
 grafo['titulo'] = "Koonfia_Social"
 exit = False
 fig, ax = plt.subplots(figsize=(15, 15))
 
+'''Adicionar vértices ao Grafo'''
+
 
 def adicionar_usuario():
+    '''Adiciona pessoas sem problema algum,porém o ideal é buscar uma forma de combater a duplicidade de usuários'''
     print("Insira o nome do usuário")
     username = input("> ")
     grafo.add_vertex(name=username)
-    print("Usuário "+username+" adicionado!")
+    print("Usuário: "+username+" adicionado!")
+
+
+'''Adicionar arestas ao Grafo'''
 
 
 def adicionar_conexao():
-    edges = [(0, 1), (0, 2), (0, 3), (0, 4), (1, 2), (1, 3), (1, 4), (3, 4)]
-    grafo.add_edges(edges)
-    """""
-    print("Quantas conexões você quer fazer?")
-    num_conexoes = input("> ")
-    while (num_conexoes > 0):
-        grafo.add_edge()
-        num_conexoes = num_conexoes-1
-        print("Deu bom")
-    """
+    '''Funciona, porém preciso resolver o loop'''
+    aresta1 = int(input("Insira o ponto de partida: "))
+    aresta2 = int(input("Insira o ponto de chegada : "))
+    grafo.add_edge(aresta1, aresta2)
+
+
+def mostrar_grafo():
+    print("Qual o tipo de visualização você deseja?")
+    print("1 - CIRCULAR")
+    print("2 - KAMADA KAWAI")
+    print("3 - TREE")
+    response = int(input("Escolha a opção: > "))
+
+    match response:
+        case 1:
+            mostrar_grafo_circular()
+        case 2:
+            mostrar_grafo_kamada_kawai()
+        case 3:
+            mostrar_grafo_tree()
+        case _:
+            print("Opção inválida!")
 
 
 def mostrar_grafo_circular():
-    print("VISUALIZAÇÃO DA REDE DE FORMA CIRCULAR\n\n")
+    print("VISUALIZAÇÃO DA REDE DE FORMA: CIRCULAR\n\n")
     ig.plot(
         grafo,
         target=ax,
@@ -41,27 +60,25 @@ def mostrar_grafo_circular():
     plt.show()
 
 
-""""
-def mostrar_grafo_star():
-    print("VISUALIZAÇÃO DA REDE DE FORMA DE ESTRELA\n\n")
+def mostrar_grafo_kamada_kawai():
+    print("VISUALIZAÇÃO DA REDE DE FORMA: KAMADA KAWAI\n\n")
     ig.plot(
         grafo,
         target=ax,
-        layout="star",
+        layout="kamada_kawai",
         vertex_size=0.1,
         vertex_color="steelblue",
         vertex_frame_color="white",
         vertex_label=grafo.vs["name"])
     plt.show()
-"""
 
 
-def mostrar_grafo_kamada_kawai():
-    print("VISUALIZAÇÃO DA REDE DE FORMA kamada_kawai\n\n")
+def mostrar_grafo_tree():
+    print("VISUALIZAÇÃO DA REDE DE FORMA: TREE\n\n")
     ig.plot(
         grafo,
         target=ax,
-        layout="kamada_kawai",
+        layout="tree",
         vertex_size=0.1,
         vertex_color="steelblue",
         vertex_frame_color="white",
@@ -79,14 +96,15 @@ def pause_system():
 
 while True:
     clear_system()
-    print("REDE SOCIAL")
+    print("=-=-=-REDE KOONFIA-=-=-=")
+    print("=-=-=-=-=-=-=-=-=-=-=-=-")
     print("1 - Adicionar usuário")
     print("2 - Adicionar conexão")
     print("3 - Propriedades da rede")
-    print("4 - Visualizar circular")
-    print("5 - Visualizar estrela")
-    print("6 - Visualizar kamada_kawai")
-    print("7 - Sair")
+    print("4 - Visualizar a rede")
+    print("5 - Importar grafo")
+    print("6 - Sair")
+    print("=-=-=-=-=-=-=-=-=-=-=-=-")
     response = int(input("Escolha a opção: > "))
 
     match response:
@@ -95,10 +113,8 @@ while True:
         case 2:
             adicionar_conexao()
         case 4:
-            mostrar_grafo_circular()
+            mostrar_grafo()
         case 6:
-            mostrar_grafo_kamada_kawai()
-        case 7:
             exit = True
         case _:
             print("Opção inválida!")
